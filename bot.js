7cfa0962b4bd0f3e96effa3e90cb4ba5deedae6b
spamzeko <44967903+spamzeko@users.noreply.github.com>
2018-11-12 15:18:58 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,11 +4,11 @@ console.log("scrpit By Golden");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("508949968363585540")
+let channel =     client.channels.get("511496928832389121")
 setInterval(function() {
 channel.send(`كيكي دو يو لوف مي`);
 }, 25)
 })
  
  
-client.login('NTA4OTUzOTM5NzEwOTY3ODA5.DsGwgw.Tmwqvp2KmGqdBtW7IlUpHUbEEI0');
+client.login('NTExNDk4OTQ5Mjk1NjAzNzIy.DsryMg.SM-hRTBdxcrt-na4aV7mBiNSr8Y');
